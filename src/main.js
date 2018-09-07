026656f990124a8ad5ccb35d0fd86dddea9dc80d
Pyragon <eldo.imo.rs@hotmail.com>
2018-09-07 02:45:16 -0700

    Add additional default telemetry events.

--- src/main.js
+++ src/main.js
@@ -6,6 +6,23 @@ var _telemetry = () => {
         name: 'logged_in',
         replace: true,
         callback: () => console.log('Logged in - plugin')
+    }, {
+        name: 'logged_out',
+        replace: true,
+        callback: () => console.log('Logged out - plugin')
+    }, {
+        name: 'token_expiry',
+        replace: true,
+        callback: () => {
+            sendNotification('Token expiring soon', 'Your authentication is about to expire. If you do not have auto login enabled, click here to relogin', () => {
+                switchToLogin();
+            });
+            console.log('Token is about to expire.');
+        }
+    }, {
+        name: 'server_restart',
+        replace: true,
+        callback: (data) => sendNotification(`Server update has begun. Server will restart in ${data.delay} seconds.`)
     }];
 
     var accepted;
